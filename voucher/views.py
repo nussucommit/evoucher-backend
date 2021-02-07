1f8f2eea56f3581dd70f87a2c9739e26e8c4621a
caitlinjee <caitlinjeespn@gmail.com>
2021-02-07 16:34:22 +0800

    Add permission classes to voucher

--- voucher/views.py
+++ voucher/views.py
@@ -1,15 +1,24 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 # Create your views here.
 from voucher.models import Voucher
 from voucher.serializers import VoucherSerializer
 
 
-class VoucherList(generics.ListCreateAPIView):
+class CreateVoucherList(generics.ListCreateAPIView):
     queryset = Voucher.objects.all()
     serializer_class = VoucherSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class VoucherList(generics.ListAPIView):
+    queryset = Voucher.objects.all()
+    serializer_class = VoucherSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class VoucherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Voucher.objects.all()
     serializer_class = VoucherSerializer
+    permission_classes = (IsAuthenticated,)
