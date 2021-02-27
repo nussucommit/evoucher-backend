922dc55505acfde18f108bb7477414df8d1a32ef
Chong Jing Quan <jingquan0302@gmail.com>
2021-02-28 02:23:15 +0800

    Implement upload image feature

--- app/backend/evoucher/settings.py
+++ app/backend/evoucher/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'evoucher.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db-image.sqlite3',
     }
 }
 
@@ -130,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
