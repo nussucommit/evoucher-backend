0528bf7503c19b09f62b5e562a029a3b44fd85a9
Chong Jing Quan <jingquan0302@gmail.com>
2021-03-21 21:23:35 +0800

    Fix typo

--- app/frontend/src/app/model-service/voucher/voucher.service.ts
+++ app/frontend/src/app/model-service/voucher/voucher.service.ts
@@ -13,7 +13,7 @@ export class VoucherService {
 
   // this method shall return vouchers that are not expired to reduce data sending overhead.
   getVouchersWithoutFilter(page: number): Observable<any> {
-    return this.http.get(`${this.baseUrlVoucher}`, { params: { page: page + '', page_size: '10' } });
+    return this.http.get(`${this.baseUrlVoucher}/`, { params: { page: page + '', page_size: '10' } });
   }
 
   getVoucherList(filterParams: any): Observable<any> {
