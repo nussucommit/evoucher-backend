361ae2e289fd9bd8c0df2c042d236e147a70ecf7
NicholasCF <nicholas.fernando@u.nus.edu>
2021-04-11 19:40:06 +0800

    Fix backend url

--- app/frontend/src/environments/environment.prod.ts
+++ app/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  frontendUrl: 'https://evoucher-frontend-test.herokuapp.com/api/'
+  frontendUrl: 'https://evoucher-backend-test.herokuapp.com/api/'
 };
