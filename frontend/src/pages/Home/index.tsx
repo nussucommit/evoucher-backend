165b0dcff8d518213ec53c8e628ac61abb32c8cc
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-07-09 22:25:14 +0800

    Added new Table component

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -6,16 +6,52 @@ import { logout } from "api/auth";
 import { getToken } from "utils/auth";
 
 import { Button } from "@commitUI/index";
+import Table from "components/Table";
 
 const Home = () => {
     const { logout: localLogout } = useAuth();
-    const res = useStudents();
-    console.log(res);
+    const dataSource = [
+        {
+            key: "1",
+            name: "Mike",
+            age: 32,
+            address: "10 Downing Street",
+        },
+        {
+            key: "2",
+            name: "John",
+            age: 42,
+            address: "10 Downing Street",
+        },
+        {
+            key: "3",
+            name: "Jane",
+            age: 42,
+            address: "10 Downing Street",
+        },
+    ];
+    const columns = [
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Age",
+            dataIndex: "age",
+            key: "age",
+        },
+        {
+            title: "Address",
+            dataIndex: "address",
+            key: "address",
+        },
+    ];
 
     return (
         <div>
             <h1>Home</h1>
-            <p>{JSON.stringify(res.data)}</p>
+            <Table dataSource={dataSource} columns={columns} />;
             <Button
                 onClick={() => {
                     const token = getToken();
