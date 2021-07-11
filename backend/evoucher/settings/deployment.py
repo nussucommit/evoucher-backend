e32717198090eeed5db8f3481824d48a9525660e
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-07-11 18:40:18 +0800

    allow all cors

--- backend/evoucher/settings/deployment.py
+++ backend/evoucher/settings/deployment.py
@@ -20,8 +20,10 @@ DATABASES = dict()
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # CORS settings
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
     os.getenv('EVOUCHER_FRONT_END_DOMAIN'),
 )
 
