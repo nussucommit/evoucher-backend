42d5cf1d798419277640995a6ab9ce2ab6260f55
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-07-11 08:56:44 +0800

    disable cors blacklist

--- backend/evoucher/settings/deployment.py
+++ backend/evoucher/settings/deployment.py
@@ -20,9 +20,10 @@ DATABASES = dict()
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # CORS settings
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:3000',
     'http://localhost:3000', # temporary
     os.getenv('EVOUCHER_FRONT_END_DOMAIN'),
 )
