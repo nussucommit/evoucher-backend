02e2a26e4607df669084f01e0c639adc904e91d9
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-09-02 16:04:11 +0800

    Add is_first_time_login field to Organization model

--- backend/organization/models.py
+++ backend/organization/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(primary_key=True, max_length=128)
     username = models.CharField(max_length=128, blank=True)
+    is_first_time_login = models.BooleanField(default=True)
