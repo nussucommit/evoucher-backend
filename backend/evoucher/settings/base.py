9e83e26f2e221b4add3bc7de05d1cb4965fb7b28
DiiSollertia <xz.khoo.work@gmail.com>
2022-02-05 21:33:53 +0800

    gmail smtp settings

--- backend/evoucher/settings/base.py
+++ backend/evoucher/settings/base.py
@@ -153,3 +153,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, '../../../static')
+
+# SMTP settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = smtp.gmail.com
+
+EMAIL_USE_TLS = True
+
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = nussu.evoucher.dev@gmail.com
+
+EMAIL_HOST_PASSWORD = vzdceuzcmayocdqk
