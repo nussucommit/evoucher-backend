00374e3d484465eac11b40d485fa995e165425fb
DiiSollertia <xz.khoo.work@gmail.com>
2022-02-05 22:11:41 +0800

    formatting issues with smtp settings

--- backend/evoucher/settings/base.py
+++ backend/evoucher/settings/base.py
@@ -156,12 +156,13 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, '../../../static')
 
 # SMTP settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = smtp.gmail.com
+
+EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_USE_TLS = True
 
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = nussu.evoucher.dev@gmail.com
+EMAIL_HOST_USER = 'nussu.evoucher.dev@gmail.com'
 
-EMAIL_HOST_PASSWORD = vzdceuzcmayocdqk
+EMAIL_HOST_PASSWORD = 'vzdceuzcmayocdqk'
